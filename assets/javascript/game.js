0ae6ffd817ca8a8d95f17adc9d8848eb2a28650c
Keo <kthorng@gmail.com>
2018-09-07 11:58:36 -0700

    final script

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,30 +5,8 @@ var losses = 0;
 var targetNumber = [];
 var counter = 0;
 
-
-function startGame() {
-
-    // Our randomly choosen number to guess
-function generate() {
-    targetNumber = (Math.floor(Math.random() * 120) + 19);
-}
-
-    generate();
-    crystalGenerate();
-    counter = 0;
-    $('#wins-text').text(wins);
-    $('#losses-text').text(losses);
-    $('#counter-text').text(counter);
-}
-
-
-// call the function
-// generate();
-// Write text to our empty div
-// $('#number').text(targetNumber)
-
 // our function to generate 4 random numbers
-function crystalGenerate() {
+var crystalGenerate = function() {
     crystalNumber1 = [Math.floor(Math.random() * 12) + 1];
     crystalNumber2 = (Math.floor(Math.random() * 12) + 1);
     crystalNumber3 = (Math.floor(Math.random() * 12) + 1);
@@ -36,9 +14,20 @@ function crystalGenerate() {
     crystalNumber5 = (Math.floor(Math.random() * -12) + -1);
     crystalNumber6 = (Math.floor(Math.random() * -12) + -1);
     crystalNumber7 = (Math.floor(Math.random() * -12) + -1);
+    // console.log('-------------');
+    // console.log(crystalNumber1);
+    // console.log(crystalNumber2);
+    // console.log(crystalNumber3);
+    // console.log(crystalNumber4);
+    // console.log(crystalNumber5);
+    // console.log(crystalNumber6);
+    // console.log(crystalNumber7);
+    // console.log('-------------');
 }
+
 // call the funciton
 crystalGenerate();
+
 // define our randomCrystal variables
 var randomCrystal1 = $('<img>');
 var randomCrystal2 = $('<img>');
@@ -79,6 +68,7 @@ $('#crystal4').append(randomCrystal4)
 $('#crystal5').append(randomCrystal5)
 $('#crystal6').append(randomCrystal6)
 $('#crystal7').append(randomCrystal7)
+
 // On click funciton to initiate our points
 $('.crystal-image').on('click', function() {
 
@@ -89,35 +79,62 @@ $('.crystal-image').on('click', function() {
     // alert('New score: ' + counter);
      
 // create an if else statement that will compare our score with random number, will push wins or losses, alert user if they lost or won, reset game when turn is up
+    // if we win by matching our targetNumber
     if (counter === targetNumber) {
         wins++;
-        startGame();
         alert('Impossible!  What is this magic you use to defeat me?  Your power levels are unreadable.');
         $('#number').text(targetNumber)
         var wish = prompt('What do you wish?');
         alert(wish + ' ' + 'has been granted...');
+        startGame();
     }
-
+    // if we loose by going over our targetNumber
     else if (counter >= targetNumber) {
         losses++;
-        startGame();
         alert('Try again Mortal, you were\'t even close.');
         $('#number').text(targetNumber)
+        startGame();
     }
-
+    // if we loose by going below 0
     else if (counter <= -1) {
         losses++;
-        startGame();
         alert('No no, we can\'t go below zero.  You\'re a rebellious little one.');
         $('#number').text(targetNumber)
+        startGame();
     }
-
+    // push our curent score to the counter text
     $('#counter-text').text(counter);
-    $('#number').text(targetNumber)
-// console.log our current score.
-    console.log(counter);
+    // push our current targetNumber to the target text
+    $('#number').text(targetNumber);
+
+    // console.log our current score.
+        // console.log('*********')
+        // console.log(counter);
+        // console.log('*********')
 });
+// our startGame variable
+var startGame = function() {
 
+    // Our randomly choosen number to guess
+var generate = function() {
+    targetNumber = (Math.floor(Math.random() * 120) + 19);
 
+}
+    //recall our functions, push text, and attributes 
+    generate();
+    crystalGenerate();
+    counter = 0;
+    $('#wins-text').text(wins);
+    $('#losses-text').text(losses);
+    $('#counter-text').text(counter);
+    randomCrystal1.attr('crystalvalue', crystalNumber1);
+    randomCrystal2.attr('crystalvalue', crystalNumber2);
+    randomCrystal3.attr('crystalvalue', crystalNumber3);
+    randomCrystal4.attr('crystalvalue', crystalNumber4);
+    randomCrystal5.attr('crystalvalue', crystalNumber5);
+    randomCrystal6.attr('crystalvalue', crystalNumber6);
+    randomCrystal7.attr('crystalvalue', crystalNumber7);
+}
+// initial game start and targetNumber text push
 startGame();
 $('#number').text(targetNumber);
